185ee4ecfe60670c3e0972c0549388a0187c28b5
moloty-69 <moloty-69@yandex.ru>
2024-05-11 21:41:44 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,22 @@
+x = 2
+y = x * x + 1
+is_big = x >= 3000
+
+x = [-1]
+print (x)
+my_list = [2,3,4,5,6]
+
+x , y = 3 , 8
+if x ==  3:
+  print (42)
+
+if x < 0:
+    if y > 0:
+        print ("налево")
+    else:
+        print ("направо")
+else:
+     print ("стой")
+
+
+  
